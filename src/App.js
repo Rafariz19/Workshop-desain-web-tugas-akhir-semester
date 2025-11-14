035f59bc6a47b41effe1234361a526577a2dd15c
Rafariz19 <firdausifairuz87@gmail.com>
2025-11-15 03:15:57 +0700

    modif app js dan app css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  // state untuk menyimpan jumlah klik
+  const [count, setCount] = React.useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Selamat Datang di Website React Pertamaku!</h1>
+      <p>Kamu sudah menekan tombol ini {count} kali.</p>
+      <button onClick={() => setCount(count + 1)}>
+        Klik Aku
+      </button>
     </div>
   );
 }
